d6a60131b6b18b0a005f569b6b32a2b658ebf363
unknown <shotvideo25@gmail.com>
2021-12-30 14:54:41 +0200

    3 axes

--- src/views/games/GamblingView.js
+++ src/views/games/GamblingView.js
@@ -1,8 +1,39 @@
+import { useState } from 'react';
 
 const GamblingView = () => {
+  const [ gameState, setGameState ] = useState([
+    { id: 1,
+      value: 0
+    },
+    { id: 2,
+      value: 0
+    },
+    { id: 3,
+      value: 0
+    }
+  ]);
+
+  const onPlayHandler = () => {
+    console.log('wow')
+    const newGameState = gameState.map( (el) => {
+      return {
+        ...el,
+        value: Math.floor(Math.random() * 10)
+      };
+    });
+    console.log('newGameState', newGameState);
+    setGameState(newGameState);
+  }
 
   return (
-    <div>Gambling view</div>
+    <div>
+      {gameState.map( (el, index) => (
+        <div key={`gambling${index}`}>
+          {el.value}
+        </div>
+      ))}
+      <button onClick={() => onPlayHandler()}>Play</button>
+    </div>
   )
 };
 
